615f69d8bf07ef42f20751555b704abd475ed4a9
Lokit sushruth Thirukumaran <97429548+lokitsushruth@users.noreply.github.com>
2023-08-14 19:52:54 +0530

    Create merge_strings_alternaltively.cpp

---
+++ merge_strings_alternaltively.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int maxLength = max(len1, len2);
+
+        for (int i = 0; i < maxLength; ++i) {
+            if (i < len1) {
+                result += word1[i];
+            }
+            if (i < len2) {
+                result += word2[i];
+            }
+        }
+
+        return result;
+    }
+};
